362014681d3fc10bdd99277788fc0d0ada8c6640
Piotr Antkiewicz <55496699+piotrantkiewicz@users.noreply.github.com>
2024-09-03 12:58:08 +0200

    connect moviesvc to navigation controller

--- Modules/Sources/MBMovies/MoviesViewController.swift
+++ Modules/Sources/MBMovies/MoviesViewController.swift
@@ -5,6 +5,13 @@ import SnapKit
 public class MoviesViewController: UIViewController {
     public override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
     }
 }
 
+extension MoviesViewController {
+    
+    private func setupUI() {
+        view.backgroundColor = .red
+    }
+}
--- MoviesBrowser/SceneDelegate.swift
+++ MoviesBrowser/SceneDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import MBMovies
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -6,10 +7,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        
+        let controller = MoviesViewController()
+        let nav = UINavigationController(rootViewController: controller)
+        window.rootViewController = nav
+        window.makeKeyAndVisible()
+        self.window = window
         
         let accessToken = Bundle.main.object(forInfoDictionaryKey: "TMDBAPIAccessToken") as! String
         let config = TMDBNetworkConfig(accessToken: accessToken)
